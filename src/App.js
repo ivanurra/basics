a5d9e130a0f329367ed28bbd801a5e6275a5bfb8
Ivan Urra <ivan.urra.wd@gmail.com>
2021-07-26 05:58:03 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+
+  const [ products, setProducts] = useState([
+    { id: 1, name: 'tshirt', price: 30},
+    { id: 2, name: 'jeans', price: 60},
+    { id: 3, name: 'hut', price: 50},
+    { id: 4, name: 'shoes', price: 80},
+  ]);
 
   const date = new Date().getFullYear();
 
